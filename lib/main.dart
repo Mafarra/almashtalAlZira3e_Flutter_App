e1c70270ca382008767ae815ec5705ff1cc61496
Mfarra <mam.farra@gmail.com>
2020-12-27 01:11:11 +0300

    message

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: Depatrments(),
+      home: MyHomePage(),
     );
   }
 }
